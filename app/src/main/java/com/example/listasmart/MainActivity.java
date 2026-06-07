73efa4048c8915673967bcd6212e58ddedd02469
Camila Basso <basso.c985@unesc.net>
2026-06-06 22:13:25 -0300

    Iniciando implementação da tela de produtos e revisando tela inicial

--- app/src/main/java/com/example/listasmart/MainActivity.java
+++ app/src/main/java/com/example/listasmart/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.listasmart;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -21,6 +23,10 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    private RecyclerView recyclerView;
+    private ProductListAdapter adapter;
+    private ArrayList<Product> produtos;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,57 +34,64 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         DBOpenHelper dbHelper = new DBOpenHelper(this);
-        dbHelper.getWritableDatabase();
 
-        MaterialButton btnLista = findViewById(R.id.btnLista);
+        ImageButton myListBtn = findViewById(R.id.mylistBtn);
+        MaterialButton btnSaibaMais = findViewById(R.id.btnSaibaMais);
+        TextView cartCount = findViewById(R.id.cartCount);
 
-        btnLista.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, MyListActivity.class);
-                startActivity(intent);
-            }
+        myListBtn.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, MyListActivity.class);
+            startActivity(intent);
         });
+
+        btnSaibaMais.setOnClickListener(v ->
+                Toast.makeText(
+                        MainActivity.this,
+                        "O Lista Smart é um projeto comunitário criado para ajudar consumidores a economizar nas compras do dia a dia.",
+                        Toast.LENGTH_LONG
+                ).show()
+        );
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            v.setPadding(
+                    systemBars.left,
+                    systemBars.top,
+                    systemBars.right,
+                    systemBars.bottom
+            );
             return insets;
         });
 
-        ArrayList<Product> produtos = new ArrayList<>();
+        produtos = new ArrayList<>();
 
-        produtos.add(
-                new Product(
-                        "Arroz",
-                        "Tio João",
-                        "Alimentos",
-                        1
-                )
-        );
+        produtos.add(new Product(
+                "Arroz",
+                "Tio João",
+                "Alimentos",
+                1
+        ));
 
-        produtos.add(
-                new Product(
-                        "Leite",
-                        "Parmalat",
-                        "Bebidas",
-                        3
-                )
-        );
+        produtos.add(new Product(
+                "Leite",
+                "Parmalat",
+                "Bebidas",
+                3
+        ));
 
-        produtos.add(
-                new Product(
-                        "Sabonete",
-                        "Dove",
-                        "Higiene",
-                        2
-                )
-        );
+        produtos.add(new Product(
+                "Sabonete",
+                "Dove",
+                "Higiene",
+                2
+        ));
 
-        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+        cartCount.setText(String.valueOf(produtos.size()));
+
+        recyclerView = findViewById(R.id.recyclerView);
         recyclerView.setLayoutManager(new GridLayoutManager(this, 2));
 
-        ProductListAdapter adapter = new ProductListAdapter(produtos);
+        adapter = new ProductListAdapter(produtos);
         recyclerView.setAdapter(adapter);
     }
 }
---
+++ app/src/main/java/com/example/listasmart/ProductPageActivity.java
@@ -0,0 +1,4 @@
+package com.example.listasmart;
+
+public class ProductPageActivity {
+}
