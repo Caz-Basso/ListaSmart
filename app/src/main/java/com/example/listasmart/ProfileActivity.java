e2255a6acadc18761c5199718cc8bb94223ce2f9
Camila Basso <basso.c985@unesc.net>
2026-06-14 13:28:24 -0300

    Iniciando implementação do perfil

--- app/src/main/java/com/example/listasmart/ProfileActivity.java
+++ app/src/main/java/com/example/listasmart/ProfileActivity.java
@@ -1,6 +1,10 @@
 package com.example.listasmart;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,5 +13,30 @@ public class ProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        ImageButton backBtn = findViewById(R.id.backBtn);
+        Button logoutBtn = findViewById(R.id.logoutBtn);
+
+        backBtn.setOnClickListener(v -> finish());
+
+        logoutBtn.setOnClickListener(v -> {
+
+            SharedPreferences prefs =
+                    getSharedPreferences("listasmart", MODE_PRIVATE);
+
+            prefs.edit()
+                    .clear()
+                    .apply();
+
+            Intent intent = new Intent(ProfileActivity.this, LoginActivity.class);
+
+            intent.addFlags(
+                    Intent.FLAG_ACTIVITY_NEW_TASK |
+                            Intent.FLAG_ACTIVITY_CLEAR_TASK
+            );
+
+            startActivity(intent);
+            finish();
+        });
     }
 }
